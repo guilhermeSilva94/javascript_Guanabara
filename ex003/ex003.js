776f4b766b88c64190b77806175ecc7879d95873
Guilherme Silva <guidss3394@gmail.com>
2022-09-10 19:54:50 -0300

    Exercício 3

--- ex003/ex003.js
+++ ex003/ex003.js
@@ -1,2 +1,4 @@
-var nome = window.prompt('Qual é seu nome?');
-window.alert('Olá ' + nome);
+var n1 = Number.parseFloat(window.prompt('Digite um número'));
+var n2 = Number.parseFloat(window.prompt('Digite outro número'));
+var soma = n1 + n2;
+window.alert(`A soma entre ${n1} e ${n2} é igual a ${soma}.`);
